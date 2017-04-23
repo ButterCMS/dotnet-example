a0f3609fe8206ade32e1d23e49c7a1c7c8f58e8c
Ross Tollefson <ross@past9systems.com>
2017-04-22 23:41:12 -0600

    Refactoring and simplification

--- ButterCmsExample/Controllers/HomeController.cs
+++ ButterCmsExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ButterCMS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,37 +8,52 @@ using System.Web.Mvc;
 
 namespace ButterCmsExample.Controllers
 {
-    public class HomeController : AppController
+    public class HomeController : Controller
     {
+        private ButterCMSClient Client;
+
         public HomeController()
         {
-
+            Client = new ButterCMSClient("b60a008584313ed21803780bc9208557b3b49fbb");
         }
 
-        public ActionResult Index()
+        [Route("")]
+        [Route("page/{page}")]
+        public async Task<ActionResult> Index(int page = 1)
         {
-            return View();
-        }
+            var response = await Client.ListPostsAsync(page);
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public async Task<ActionResult> Blog()
-        {
-            var response = await Client.ListPostsAsync();
             ViewBag.Posts = response.Data;
-            return View();
+
+            if (response.Meta.NextPage.HasValue)
+            {
+                ViewBag.HasNext = true;
+                ViewBag.NextPage = response.Meta.NextPage;
+            }
+            else
+            {
+                ViewBag.HasNext = false;
+            }
+
+            if (response.Meta.PreviousPage.HasValue)
+            {
+                ViewBag.HasPrevious = true;
+                ViewBag.PreviousPage = response.Meta.PreviousPage;
+            }
+            else
+            {
+                ViewBag.HasPrevious = false;
+            }
+
+            return View("~/Views/Index.cshtml");
+        }
+
+        [Route("post/{slug}")]
+        public async Task<ActionResult> Post(string slug)
+        {
+            var response = await Client.RetrievePostAsync(slug);
+            ViewBag.Post = response.Data;
+            return View("~/Views/Post.cshtml");
         }
 
     }
