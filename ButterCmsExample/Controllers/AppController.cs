6fc90cac590084ad1f00ab7a939667f21e80accc
Ross Tollefson <ross@past9systems.com>
2017-04-22 22:26:23 -0600

    Initial commit, create MVC project, add ButterCMS NuGet package, add base controller

---
+++ ButterCmsExample/Controllers/AppController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ButterCMS;
+
+namespace ButterCmsExample.Controllers
+{
+    public abstract class AppController : Controller
+    {
+        private string _authToken = "b60a008584313ed21803780bc9208557b3b49fbb";
+
+        protected ButterCMSClient Client => new ButterCMSClient(_authToken);
+
+    }
+}
